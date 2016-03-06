97a20492e2e1d1a1d0f260cdfa1079e03018896b
David Michael Weis <dweis7@gmail.com>
2016-03-06 23:26:01 +0100

    initial commit

---
+++ src/webbotLejos/Robot.java
@@ -0,0 +1,49 @@
+package webbotLejos;
+
+import lejos.nxt.NXTRegulatedMotor;
+
+public class Robot
+{
+   private NXTRegulatedMotor rightMotor;
+   private NXTRegulatedMotor leftMotor;
+   
+   public Robot(NXTRegulatedMotor rightMotor, NXTRegulatedMotor leftMotor)
+   {
+      this.leftMotor = leftMotor;
+      this.rightMotor = rightMotor;
+   }
+   
+   public void forward(){
+      rightMotor.forward();
+      leftMotor.forward();
+   }
+
+   public void backward(){
+      rightMotor.backward();
+      leftMotor.backward();
+   }
+   
+   public void turnLeft()
+   {
+      rightMotor.forward();
+      leftMotor.backward();
+   }
+   
+   public void turnRight()
+   {
+      leftMotor.forward();
+      rightMotor.backward();
+   }
+   
+   public void stop()
+   {
+      leftMotor.stop();
+      rightMotor.stop();
+   }
+   
+   public void flt()
+   {
+      leftMotor.flt();
+      rightMotor.flt();
+   }
+}
