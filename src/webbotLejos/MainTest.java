47a60ce31cc80a2fc5ae0a8b9984911ab4b3cec0
David Weis <dweis7@gmail.com>
2016-04-25 13:17:19 +0200

    made temporary main class

---
+++ src/webbotLejos/MainTest.java
@@ -0,0 +1,17 @@
+package webbotLejos;
+
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		Robot robot = new Robot(Motor.B, Motor.A);
+		SensorHandler sensorHandler = new SensorHandler(SensorPort.S3, SensorPort.S4, SensorPort.S2, SensorPort.S1);
+		RemoteController remote = new RemoteController(sensorHandler, robot);
+		remote.connect();
+		remote.run();
+	}
+
+}
--- src/webbotLejos/RemoteController.java
+++ src/webbotLejos/RemoteController.java
@@ -2,6 +2,8 @@ package webbotLejos;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import lejos.nxt.LCD;
 import lejos.nxt.comm.Bluetooth;
@@ -13,7 +15,7 @@ public class RemoteController {
 
 	private NXTConnection connection;
 	private DataOutputStream output;
-	private DataInputStream input;
+	private InputStream input;
 
 	public RemoteController(SensorHandler sensorHandler, Robot robot) {
 		this.sensorHandler = sensorHandler;
@@ -28,7 +30,27 @@ public class RemoteController {
 		robot.display("Waiting for connection");
 		connection = Bluetooth.waitForConnection(0, NXTConnection.RAW);
 		robot.display("Connected");
-		input = connection.openDataInputStream();
+		input = connection.openInputStream();
 		output = connection.openDataOutputStream();
+	}
+	
+	public void run()
+	{
+		while(true)
+		{
+			try {
+				int command = input.read();
+				switch (command)
+				{
+				case 0 : robot.display("Reading not implemented yet"); break;
+				case 1 : robot.forward(); break;
+				case 2 : robot.backward(); break;
+				case 3 : robot.turnLeft(); break;
+				case 4 : robot.turnRight(); break;
+				case 5 : robot.stop(); break;
+				case 6 : robot.flt(); break;
+				}
+			} catch (IOException e) {}
+		}
 	}
 }
