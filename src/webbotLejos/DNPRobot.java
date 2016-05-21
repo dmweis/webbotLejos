237909897cd7c3a8beaccffa608a012501ca01ae
David Michael Weis <dweis7@gmail.com>
2016-05-21 15:21:17 +0200

    renamed class and added comments
    also changed robot from stopping if not connected to float since it
    drains less battery

--- src/webbotLejos/DNPRobot.java
+++ src/webbotLejos/DNPRobot.java
@@ -3,8 +3,12 @@ package webbotLejos;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 
-public class MainTest {
+public class DNPRobot {
 
+	/**
+	 * main method that starts connection and keeps restarting it if it fails
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Robot robot = new Robot(Motor.B, Motor.A);
 		SensorHandler sensorHandler = new SensorHandler(SensorPort.S3, SensorPort.S4, SensorPort.S2, SensorPort.S1);
@@ -12,7 +16,7 @@ public class MainTest {
 		while(true){
 			if (remote.connect()) {
 				remote.run();
-				robot.stop();
+				robot.flt();
 			}
 		}
 	}
